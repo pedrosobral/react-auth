792e2b4981845cfaf7057c5dfc82707c785a124d
Pedro Henrique <pedrosobralxv@gmail.com>
2018-06-17 01:09:19 -0300

    feat(card-section): add component

---
+++ src/components/common/CardSection.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { StyleSheet, View } from 'react-native';
+
+interface Props {
+  children?: React.ReactNode[];
+}
+
+function CardSection({ children }: Props) {
+  return (
+    <View style={styles.section}>
+      {children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  section: {
+    backgroundColor: '#fff',
+    borderBottomWidth: 0,
+    borderColor: '#ddd',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    padding: 5,
+    position: 'relative',
+  },
+});
+
+export { CardSection };
